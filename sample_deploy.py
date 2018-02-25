d2886391566fb091dedffdef27ada151c45ecd34
Evgeniy Kozhuhovskiy <ugenk@mediatech.by>
2018-02-25 20:41:08 +0530

    missed HEADERS variable

--- sample_deploy.py
+++ sample_deploy.py
@@ -38,7 +38,13 @@ if mode == 'production':
 
 try: DATA
 except NameError: sys.exit(1)
-
+HEADERS = {
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+	'Accept-Encoding': 'gzip, deflate, sdch',
+	'Accept-Language': 'en,en-US;q=0.8,ru;q=0.6,uk;q=0.4',
+	'Connection': 'keep-alive'
+}
 response = requests.post(URL, headers=HEADERS, data=DATA)
 response.content
 data=json.loads(json.dumps(response.json()))
